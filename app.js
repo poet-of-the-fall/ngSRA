d252110c21d2f7ea0cc40705f507820c433f2828
Danilo Gasdzik <poet_of_the_fall@gmx.de>
2025-01-31 17:22:12 +0100

    fixed fullscreen

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const {app, BrowserWindow} = require('electron')
       mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        fullscreen: true,
         webPreferences: {
           nodeIntegration: true
         }
       })
+
+      mainWindow.maximize();
 
       mainWindow.loadURL(
         url.format({
